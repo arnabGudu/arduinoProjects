1bd383fa1729155629a62601fe34a5094263afd8
arnabGudu <arnabgudu.gs@gmail.com>
2019-03-13 21:56:08 +0530

    Self Balance added

---
+++ self_balance/self_balance.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+void setup() 
+{
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
+  {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+}
+
+void loop() {
+
+
+}
+
+float pitch()
+{
+  Vector normAccel = mpu.readNormalizeAccel();
+  int pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
+}
